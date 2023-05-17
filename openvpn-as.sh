52dd6cad06d2b9581a356c94b48faa19cdc9d884
parkero2 <72895391+parkero2@users.noreply.github.com>
2023-05-18 09:26:09 +1200

    Update openvpn-as.sh

--- openvpn-as.sh
+++ openvpn-as.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sudo su
+#sudo su
 apt update
 apt -y upgrade
 apt update && apt -y install ca-certificates wget net-tools gnupg
-wget -qO - https://as-repository.openvpn.net/as-repo-public.gpg | apt-key add -
+wget -qo - https://as-repository.openvpn.net/as-repo-public.gpg | apt-key add -
 echo "deb http://as-repository.openvpn.net/as/debian jammy main">/etc/apt/sources.list.d/openvpn-as-repo.list
 apt update && apt -y install openvpn-as
